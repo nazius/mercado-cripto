312c3049951c7c3edb2b9cd693b5b0da59ee9d96
jnazar <juanpablo.nazar@elliemae.com>
2020-06-01 00:53:11 -0400

    feature(buda): add buda api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import PriceTable from './components/PriceTable'
 import './App.css';
 
 function App() {
+
+  // HTML
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Mercado Cripto</h1>
       </header>
+      <div className="price-table-container">
+        <PriceTable />
+      </div>
     </div>
   );
 }
---
+++ src/components/PriceTable.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-dupe-args */
+import React, { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table'
+import Buda from 'buda-promise'
+const buda = new Buda();
+
+// FUNCION PARA BUSCAR PRECIO
+async function getBudaBTC_CLP(setPriceBudaBTC, setPriceBudaETH, setPriceBudaLTC) {
+  const tickerBTC = await buda.ticker('btc-clp')
+  const tickerETH = await buda.ticker('eth-clp')
+  const tickerLTC = await buda.ticker('ltc-clp')
+  const precioBTC = tickerBTC.ticker.last_price[0]
+  const precioETH = tickerETH.ticker.last_price[0]
+  const precioLTC = tickerLTC.ticker.last_price[0]
+  setPriceBudaBTC(precioBTC)
+  setPriceBudaETH(precioETH)
+  setPriceBudaLTC(precioLTC)
+}
+
+
+// COMPONENTE REACT
+function PriceTable() {
+  const [priceBudaBTC, setPriceBudaBTC] = useState(0)
+  const [priceBudaETH, setPriceBudaETH] = useState(0)
+  const [priceBudaLTC, setPriceBudaLTC] = useState(0)
+
+  useEffect(() => {
+    getBudaBTC_CLP(setPriceBudaBTC, setPriceBudaETH, setPriceBudaLTC)
+    setInterval(() => {
+      getBudaBTC_CLP(setPriceBudaBTC, setPriceBudaETH, setPriceBudaLTC)
+    }, 3000);
+  }, [])
+
+  return (
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Moneda</th>
+          <th>Buda</th>
+          <th>CryptoMkt</th>
+          <th>OrionX</th>
+          <th>Satoshi Tango</th>
+          <th>CoinMarketCap</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>BTC</td>
+          <td>{priceBudaBTC}</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>ETH</td>
+          <td>{priceBudaETH}</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>LTC</td>
+          <td>{priceBudaLTC}</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default PriceTable;
